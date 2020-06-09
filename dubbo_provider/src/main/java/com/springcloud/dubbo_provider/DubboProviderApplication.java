03262fc9f52cf79c653cc923ef2e2e1864601772
spike <spikezeng@etekcity.com.cn>
2020-06-09 13:33:25 +0800

    init

---
+++ dubbo_provider/src/main/java/com/springcloud/dubbo_provider/DubboProviderApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.dubbo_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DubboProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboProviderApplication.class, args);
+    }
+
+}
